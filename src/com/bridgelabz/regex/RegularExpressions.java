46dd1112af7d1deee0edb11f1d533290e525dee7
megha angadi <meghamangadi@gmail.com>
2021-09-14 22:57:49 +0530

    Added: User Message to the User

---
+++ src/com/bridgelabz/regex/RegularExpressions.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.regex;
+
+import java.util.Scanner;
+
+public class RegularExpressions {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Java RegularExpression user Registration Problems");
+	}
+
+}
