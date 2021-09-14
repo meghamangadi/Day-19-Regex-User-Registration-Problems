963b52d98d5202abcf880fdd830f20ed2f78e36c
megha angadi <meghamangadi@gmail.com>
2021-09-14 23:04:35 +0530

    Added: Program to Validate the Firstname entered by the user

--- src/com/bridgelabz/regex/RegularExpressions.java
+++ src/com/bridgelabz/regex/RegularExpressions.java
@@ -1,12 +1,31 @@
 package com.bridgelabz.regex;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegularExpressions {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Java RegularExpression user Registration Problems");
+		RegularExpressions.firstName();
 	}
 
+	public static void firstName() {
+
+		System.out.println("Enter Your Firstname");
+		String input = sc.next();
+
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(input);
+
+		if (matcher.matches()) {
+			System.out.println("Valid Firstname");
+		} else {
+			System.out.println("Invalid Firstname...!");
+			System.out.println("Your Firstname should start with capital letter and has minimum 3 characters ");
+		}
+
+	}
 }
